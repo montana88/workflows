8116b804741e12208b269f1ca04b20da8210c7c4
Toni <tmaat88@gmail.com>
2016-02-27 14:06:07 +0100

    First commit

---
+++ components/scripts/template.js
@@ -0,0 +1,11 @@
+mplate.js
+$(function() {
+    var Mustache = require('mustache');
+
+    $.getJSON('js/data.json', function(data) {
+        var template = $('#speakerstpl').html();
+        var html = Mustache.to_html(template, data);
+        $('#speakers').html(html);
+    }); //getJSON
+
+}); //function
